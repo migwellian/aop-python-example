0091d47806a6741532e93fc4365c0b78725b7085
Michael Lewin <michaellewin@improbable.io>
2019-10-26 22:15:56 +0100

    it's easy to change an aspect in one place

--- aop_example/decorators.py
+++ aop_example/decorators.py
@@ -1,7 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def logged(func):
     def wrapped(*args, **kwargs):
-        print(f"Called {func.__name__} {args} {kwargs}")
+        logging.debug(f"Called {func.__name__} {args} {kwargs}")
         func(*args, **kwargs)
 
     return wrapped
